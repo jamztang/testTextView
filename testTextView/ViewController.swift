7581d8088af7f342fd54314a285b9aa3cb71c41c
James Tang <james@goodnotesapp.com>
2020-05-27 12:43:03 +0800

    Initial Commit

---
+++ testTextView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testTextView
+//
+//  Created by James Tang on 27/5/2020.
+//  Copyright © 2020 James Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
